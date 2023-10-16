25e06cc3cba57133d37aec0a4dc26b506ff798b2
angelDova <Angelfcb01@yahoo.com>
2023-10-16 16:16:39 -0700

    first commit

---
+++ components/filters.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useState } from "react";
+
+const links = [
+  "Art of the Triangle",
+  "Coach, What Do I Do From 50/50?",
+  "All Aspects of the Armbar",
+  "Guillotine Mastery",
+  "All Access Dojo Membership/Month",
+  "All Access Dojo Membership/Year",
+];
+
+const Filters = () => {
+  const [active, setActive] = useState("");
+
+  const handleFilter = (link: string) => {
+    setActive(link);
+  };
+
+  console.log({ active });
+
+  return (
+    <ul className="text-white-800 body-text no-scrollbar flex w-full max-w-full gap-2 overflow-auto py-12 sm:max-w-2xl">
+      {links.map((link) => (
+        <button
+          key={link}
+          className={`${active === link ? "gradient_blue-purple" : ""}
+          whitespace-nowrap rounded-lg px-8 py-2.5 capitalize`}
+          onClick={() => handleFilter(link)}
+        >
+          {link}
+        </button>
+      ))}
+    </ul>
+  );
+};
+
+export default Filters;
+
+// 1:01:38
---
+++ components/navbar.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import { Equal } from "lucide-react";
+
+const Navbar = () => {
+  return (
+    <nav className="flex-center fixed- top-0 z-50 w-full border-b-2 border-black-200 py-7 text-white">
+      <div className="flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px-16">
+        <Link href="/">
+          <Image src="logo.svg" alt="Logo" width={55} height={40} />
+        </Link>
+        <Button variant="ghost" size="icon" className="block md:hidden">
+          <Equal className="h-[30px] w-[30px]" />
+        </Button>
+        <ul className="flex-center gap-x-3 max-md:hidden md:gap-x-10">
+          <li className="body-text text-gradient_blue-purple !font-bold">
+            <Link href="https://dovapro.vercel.app" target="_blank">
+              DovaPro JiuJitsu
+            </Link>
+          </li>
+          <li className="body-text text-gradient_blue-purple !font-bold">
+            <Link href="/" target="_blank">
+              DovaPro Instrucitonals
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
